54a98149d53ca2fd3f1392c01c6cc1142f3bb6af
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-29 20:21:21 -0800

    made a to do list

--- core/frontend/src/App.tsx
+++ core/frontend/src/App.tsx
@@ -2,14 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [mediaList, setMediaList] = useState<string[]>([]);
+  const [currInput, setCurrInput] = useState("");
+
+  function submit() {
+    setMediaList((mediaList) => [...mediaList, currInput]);
+    setCurrInput("");
+  }
 
   return (
     <div id="maincontent">
-      <h1>this is a react app!</h1>
-      <button onClick={() => setCount((count) => count + 5)}>
+      <h1>Welcome to SharedTropes!</h1>
+      <p>Input yr fav media below to see what tropes they share:</p>
+      <input
+        value={currInput}
+        onChange={(e) => setCurrInput(e.target.value)}
+      ></input>
+      <button onClick={submit}>submit</button>
+      <ul>
+        {mediaList.map((elt) => (
+          <li key={elt}>{elt}</li>
+        ))}
+      </ul>
+      {/* <button onClick={() => setCount((count) => count + 5)}>
         count is {count}
-      </button>
+      </button> */}
     </div>
   );
 }
