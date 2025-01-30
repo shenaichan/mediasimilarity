a9c3354fd284959663f99a4104dbdff37cfe5f65
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-30 01:30:29 -0800

    nabbed links in main trope index

--- core/scraping/main.py
+++ core/scraping/main.py
@@ -3,7 +3,30 @@ from selenium.webdriver.common.by import By
 
 # set up web driver
 driver = webdriver.Firefox()
-driver.get()
+
+
+numTropePages = 61
+# 500 tropes per page.......
+tropeIndexBaseUrl = "https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?n=Main&t=trope&page="
+
+
+def getLinks(pageNum: int):
+    driver.get(tropeIndexBaseUrl+str(pageNum))
+    # links = driver.find_elements(By.PARTIAL_LINK_TEXT, '/pmwiki/pmwiki.php/')
+    tableEntries = driver.find_elements(By.TAG_NAME, 'td')
+    tropes = []
+    for entry in tableEntries:
+        link = entry.find_element(By.XPATH, "./*")
+        tropes.append(link.text.strip())
+    return tropes
+
+
+# for i in range(1, numTropePages+1):
+#     getMediaFromTropePage(i)
+
+print(getLinks(30))
+
+driver.quit()
 
 '''
 go to tvtropes
@@ -12,7 +35,7 @@ https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?n=Main&t=t
 
 grab each link
 
-go into page, first try to open all folders
+go into page, first try to open all folders --> don't have to bc the text is already loaded onto the page lol
 get each HTML <a> element and check if the link doesn't involve /Main/ (i.e. it's a media)
 also check if links involve the link itself, then it's a subpage, and repeat the process here
 get the URL, check if it's in the DB already, and if not, add to the DB
