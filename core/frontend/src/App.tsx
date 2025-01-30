f2c14b92737749b7e56119e41401063dd40946da
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-30 00:06:37 -0800

    press enter to submit

--- core/frontend/src/App.tsx
+++ core/frontend/src/App.tsx
@@ -6,6 +6,12 @@ function App() {
   const [mediaList, setMediaList] = useState<string[]>([]);
   const [currInput, setCurrInput] = useState("");
 
+  function submitOnEnter(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      submit();
+    }
+  }
+
   function submit() {
     setMediaList((mediaList) => [...mediaList, currInput]);
     setCurrInput("");
@@ -18,8 +24,11 @@ function App() {
       <input
         value={currInput}
         onChange={(e) => setCurrInput(e.target.value)}
+        onKeyDown={submitOnEnter}
       ></input>
-      <button onClick={submit}>submit</button>
+      <button onClick={submit}>
+        click to submit or you can just press enter
+      </button>
       <ul>
         {mediaList.map((elt) => (
           <li key={elt}>{elt}</li>
