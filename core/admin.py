60f02e14e0b6c8f7aa5359086b4a28e6b5469492
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-01-31 15:32:38 -0800

    create admin remember to upload env vars

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+# from .models import User
+from .models import Trope, Media
 
-admin.site.register(User, UserAdmin)
+admin.site.register(Trope)
+admin.site.register(Media)
+
+# admin.site.register(User, UserAdmin)
