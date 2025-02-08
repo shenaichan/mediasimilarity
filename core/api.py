27b700142af02c5dcf3c7245035301abe5f13276
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-02-08 15:37:04 -0800

    more nice searchy thingsss

--- core/api.py
+++ core/api.py
@@ -1,15 +1,18 @@
 from .models import Media, Trope
+from django.db.models import Count
 from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
 class MediaSchema(Schema):
+    urlSafeTitle: str
+    urlMediaType: str
     displayTitle: str
 
 @api.get("/search", response=list[MediaSchema])
 def search(request, q: str):
-    medias = Media.objects.filter(displayTitle__icontains=q)
-    print([(med.urlSafeTitle, med.urlMediaType, med.displayTitle) for med in medias])
+    medias = Media.objects.filter(displayTitle__icontains=q).annotate(tropeCount=Count('tropes')).filter(tropeCount__gt=10).order_by('-tropeCount')
+    print([(med.urlSafeTitle, med.urlMediaType, med.displayTitle, med.tropes.count()) for med in medias])
     # print([trope.id for trope in medias[0].tropes.all()])
     # print([trope.medias.all() for trope in medias[1].tropes.all()])
 
@@ -19,4 +22,4 @@ def search(request, q: str):
     # tropes = [(trope.displayName, trope.medias.count()) for trope in med.tropes.all()]
     # # tropes.sort(key=lambda x: x[1])
     # print(tropes)
-    return medias
+    return medias[:min(len(medias), 20)]
--- core/frontend/src/App.tsx
+++ core/frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type media = {
+  urlSafeTitle: string;
+  urlMediaType: string;
+  displayTitle: string;
+};
+
 function App() {
   // const [count, setCount] = useState(0);
-  const [mediaList, setMediaList] = useState<string[]>([]);
+  const [mediaList, setMediaList] = useState<media[]>([]);
   const [currInput, setCurrInput] = useState("Search for media...");
   const [inputActivated, setInputActivated] = useState(false);
   const [timeoutId, setTimeoutId] = useState(-1);
 
-  function submitOnEnter(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === "Enter") {
-      submit();
-    }
-  }
+  // function submitOnEnter(e: React.KeyboardEvent<HTMLInputElement>) {
+  //   if (e.key === "Enter") {
+  //     submit();
+  //   }
+  // }
 
-  function submit() {
-    setMediaList((mediaList) => [...mediaList, currInput]);
-    setCurrInput("");
-  }
+  // function submit() {
+  //   setMediaList((mediaList) => [...mediaList, currInput]);
+  //   setCurrInput("");
+  // }
 
   function activate() {
-    if (!inputActivated){
+    if (!inputActivated) {
       setCurrInput("");
       setInputActivated(true);
     }
@@ -33,9 +39,10 @@ function App() {
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
-      const json = await response.json();
+      const json: media[] = await response.json();
+      setMediaList(json);
       console.log(json);
-    } catch(e) {
+    } catch (e) {
       console.error((e as Error).message);
     }
   }
@@ -51,29 +58,43 @@ function App() {
     }
   }, [currInput]);
 
+  function maybeAddType(elt: media) {
+    const addMedia =
+      mediaList.filter((item) => item.displayTitle === elt.displayTitle)
+        .length > 1;
+    if (addMedia) {
+      return `${elt.displayTitle} (${elt.urlMediaType})`;
+    } else {
+      return elt.displayTitle;
+    }
+  }
 
   return (
     <div id="maincontent">
       <div id="searchstuff">
         <h1>t(venn)tropes</h1>
         <input
-          style={inputActivated ? {} : {color: "gray", fontStyle: "italic"}}
+          style={inputActivated ? {} : { color: "gray", fontStyle: "italic" }}
           value={currInput}
           onClick={activate}
           onChange={(e) => setCurrInput(e.target.value)}
-          onKeyDown={submitOnEnter}
+          // onKeyDown={submitOnEnter}
         ></input>
         {/* <button onClick={submit}>
           click to submit or you can just press enter
         </button> */}
         <ul>
           {mediaList.map((elt) => (
-            <li key={elt}>{elt}</li>
+            <li key={`${elt.urlMediaType}/${elt.urlSafeTitle}`}>
+              <a
+                href={`https://tvtropes.org/pmwiki/pmwiki.php/${elt.urlMediaType}/${elt.urlSafeTitle}`}
+              >
+                {maybeAddType(elt)}
+              </a>
+            </li>
           ))}
         </ul>
-        <button
-          style={{margin: "5px 0px", width: "fit-content"}}
-        >
+        <button style={{ margin: "5px 0px", width: "fit-content" }}>
           Get shared tropes!
         </button>
       </div>
